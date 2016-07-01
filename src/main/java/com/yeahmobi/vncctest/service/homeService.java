bb12b7e8c280b48e3fae2228f5def56877dec53b
steven.liu <solinary@live.com>
2016-07-01 16:34:52 +0800

    rename

--- src/main/java/com/yeahmobi/vncctest/service/homeService.java
+++ src/main/java/com/yeahmobi/vncctest/service/homeService.java
@@ -1,7 +1,17 @@
 package com.yeahmobi.vncctest.service;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * Created by steven.liu on 2016/6/28.
  */
-public class homeService {
+@Controller
+public class HomeService {
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public ModelAndView showDataPage() {
+        return new ModelAndView("home");
+    }
 }
