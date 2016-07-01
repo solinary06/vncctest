21c2d2ee1c8370426f020ff9a58349d56cb3da49
steven.liu <solinary@live.com>
2016-07-01 16:28:31 +0800

    Renanme redisDao to RedisDao

--- src/test/java/StartJetty.java
+++ src/test/java/StartJetty.java
@@ -1,5 +1,30 @@
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.webapp.WebAppContext;
+
 /**
  * Created by steven.liu on 2016/4/18.
  */
 public class StartJetty {
+    public static void main(String[] args) {
+//        try {
+//            System.out.println(System.getProperty("user.dir"));
+//
+//            Server server = new Server(8080);
+//
+//            WebAppContext context = new WebAppContext();
+//            context.setDescriptor("./src/main/webapp/WEB-INF/web.xml");
+//            context.setResourceBase("./src/main/webapp");
+//            context.setContextPath("/");
+//            context.setParentLoaderPriority(true);
+//
+//            server.setHandler(context);
+//
+//            server.start();
+//            server.join();
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }
+    }
+
+
 }
--- src/test/java/StartServer.java
+++ src/test/java/StartServer.java
@@ -1,5 +1,17 @@
+//import com.yeahmobi.integrationTest.server.JettyServer;
 /**
  * Created by steven.liu on 2016/4/18.
  */
 public class StartServer {
+
+//    static JettyServer server = new JettyServer();
+//
+//    public static void main(String[] args) {
+//        try {
+//            server.startJettyServer(8080, "vncctest",null);
+//
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
 }
